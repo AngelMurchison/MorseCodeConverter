5ef7c8d6c6732c2148589843f44b35ea05e2244e
Angel Murchison <angelmurchison09@gmail.com>
2016-11-30 14:37:06 -0500

    Created a loop so user can go again or opt to quit. Level 1 Completed

--- MorseCodeConverter/Program.cs
+++ MorseCodeConverter/Program.cs
@@ -7,72 +7,54 @@ using System.IO;
 
 namespace MorseCodeConverter
 {
-    //class Morse
-    //{
-    //    //public string Code { get; set; }
-    //    //public string Letter { get; set; }
-    //    //public override string ToString()
-    //    //{
-    //    //    return $"{Letter}, {Code}";
-    //    //}
 
-    //   new Dictionary<char, string>(); 
-    //}
+
 
     class Program
     {
+        //public bool query()
+        //{
+        //    bool wanttolearn = false;
+        //    string wanttotype = Console.ReadLine().ToUpper();
+        //    if (wanttotype == "y") { wanttolearn = true; }
+        //    if (wanttotype == "n") { wanttolearn = false; }
+        //    return wanttolearn;
+
+        //}
         static void Main(string[] args)
         {
 
             var filePath = "Morse.csv";
             var usersPath = "UsersInput.csv";
-            //var allcode = new List<Morse>();
             var lettertoCode = File.ReadLines(filePath).Select(line => line.Split(',')).ToDictionary(data => data[0], data => data[1]);
-            var usersInput = new Dictionary<string, string>();
+            var usersInputKey = new List<string>();
+            var usersInputValue = new List<string>();
             string morse = string.Empty;
             bool wantToLearn = false;
-            //using (var sr = new StreamReader(filePath))
-            //{
-            //    while (sr.Peek() > 0)
-            //    {
-            //        var line = sr.ReadLine().Split(',');
-            //        //var letter = line[0];
-            //        //var code = line[1];
-            //        //var temp1 = new string { letter };
-            //        //var temp2 = new string { code };
-            //        //dictionary.Add(temp1, temp2);
-            //    }
 
-            //var character = "R";
-            //var morse = CodeToLetter[character];
-            //Console.WriteLine(morse);
 
-            //}
             Console.WriteLine($"Hello user, would you like to practice morse code? Y for yes, N for no.");
             string wantToType = Console.ReadLine().ToUpper();
-            if (wantToType == "Y"){ wantToLearn = true; }
+            if (wantToType == "Y") { wantToLearn = true; }
             if (wantToType == "N") { wantToLearn = false; }
 
-            // Loop through all the characters in the string 'input'
-            // for each character
-            //   lookup that character in CodeToLetter
-            //   print whatever we find from that lookup
             while (wantToLearn == true)
             {
                 Console.WriteLine("Please give me a letter or sentence and I'll give you each letter translated into morse code!");
-            var input = Console.ReadLine().ToUpper();
+                var input = Console.ReadLine().ToUpper();
                 foreach (char item in input)
                 {
                     var itemString = item.ToString();
                     morse = lettertoCode[itemString];
                     Console.WriteLine(morse);
-                    usersInput.Add(itemString, morse);
+                    usersInputKey.Add(itemString);
+                    usersInputValue.Add(morse);
                 }
                 using (StreamWriter sw = new StreamWriter(usersPath))
                 {
-                    foreach (var item in usersInput)
+                    foreach (var item in usersInputKey)
                     {
-                        sw.WriteLine(item);
+                        sw.WriteLine($"{item}, {lettertoCode[item]}");
                     }
                 }
                 Console.WriteLine("Would you like to translate something else?");
