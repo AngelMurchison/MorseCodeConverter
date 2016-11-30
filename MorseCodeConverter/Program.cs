ba650f682326aef1f45754eb78ab99b2a7066585
Angel Murchison <angelmurchison09@gmail.com>
2016-11-30 08:40:26 -0500

    User is being asked for input, working on printing value from that input.

--- MorseCodeConverter/Program.cs
+++ MorseCodeConverter/Program.cs
@@ -26,21 +26,32 @@ namespace MorseCodeConverter
 
             var filePath = "Morse.csv";
             //var allcode = new List<Morse>();
-            var CodeToLetter = File.ReadLines(filePath).Select(line => line.Split(',')).ToDictionary(data => char.Parse(data[0]), data => data[1]);
-            using (var sr = new StreamReader(filePath))
+            var CodeToLetter = File.ReadLines(filePath).Select(line => line.Split(',')).ToDictionary(data => data[0], data => data[1]);
+            foreach (var item in CodeToLetter)
             {
-                while (sr.Peek() > 0)
+                Console.WriteLine(item);
+            }
+            //using (var sr = new StreamReader(filePath))
+            //{
+            //    while (sr.Peek() > 0)
+            //    {
+            //        var line = sr.ReadLine().Split(',');
+            //        //var letter = line[0];
+            //        //var code = line[1];
+            //        //var temp1 = new string { letter };
+            //        //var temp2 = new string { code };
+            //        //dictionary.Add(temp1, temp2);
+            //    }
+
+            //}
+            Console.ReadLine();
+            Console.WriteLine($"Hello user, would you like to practice morse code?\nPlease give me a single character A-Z or 0-9 and I'll give you that letter translated into morse code!");
+            var input = Console.ReadLine();
+            foreach (KeyValuePair<string, string> item in CodeToLetter)
+            {
+                if (CodeToLetter.ContainsKey(input))
                 {
-                    var line = sr.ReadLine().Split(',');
-                    //var letter = line[0];
-                    //var code = line[1];
-                    //var temp1 = new string { letter };
-                    //var temp2 = new string { code };
-                    //dictionary.Add(temp1, temp2);
-                    foreach (var item in CodeToLetter)
-                    {
-                        Console.WriteLine(item);
-                    }
+                    Console.WriteLine(item.Value);
                 }
             }
             Console.ReadLine();
